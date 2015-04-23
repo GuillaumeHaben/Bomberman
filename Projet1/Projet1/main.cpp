ffd219e0b71c0259b6418c4ec0d144dc8e9a5546
GuillaumeHaben <pyxouze@gmail.com>
2015-04-23 16:04:45 +0200

    SDL - Gestion de la fermeture de la fenêtre

--- Projet1/Projet1/main.cpp
+++ Projet1/Projet1/main.cpp
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
+#include "Game.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
+
+//Event system
+SDL_Event event;
+
+void clean_up()
+{
+	//Destroy the window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL
+	SDL_Quit();
+}
+
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	//Waiting for user to quit
+	bool quit = false;
 
 	//Initialize SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -20,7 +36,7 @@ int main( int argc, char* args[] )
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		window = SDL_CreateWindow( "Bomberman", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( window == NULL )
 		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -36,16 +52,24 @@ int main( int argc, char* args[] )
 			//Update the surface
 			SDL_UpdateWindowSurface( window );
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
+			//While user hasn't quit
+			while (quit == false)
+			{
+				//While there is an event
+				while (SDL_PollEvent(&event))
+				{
+					//If user has click on the X of the window
+					if (event.type == SDL_QUIT)
+					{
+						//We close the program
+						quit = true;
+					}
+				}
+			}
+
+			clean_up();
 		}
 	}
-
-	//Destroy window
-	SDL_DestroyWindow( window );
-
-	//Quit SDL subsystems
-	SDL_Quit();
 
 	return 0;
 }
