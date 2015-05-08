58a34979c6d137a427ad2f9965fbb97a1a7c169a
ColaMaya <elise.rame@telecomnancy.net>
2015-05-08 16:47:04 +0200

    Générateur de caisses

--- Projet1/Projet1/Level.h
+++ Projet1/Projet1/Level.h
@@ -1,6 +1,9 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "Case.h"
 #include "Game.h"
 
@@ -14,6 +17,7 @@ public:
 	// Setters.
 	void setUpGame(int niveau);
 	void niveau1();
+	void generateurCaisses(int nombreCaisses);
 
 private:
 	Game* plateau;
